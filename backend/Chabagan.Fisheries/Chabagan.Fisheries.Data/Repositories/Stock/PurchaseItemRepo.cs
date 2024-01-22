b1b441209bb8106fe5255aa4c94f468689f93e9e
shohanur rahman <shohanur.rahman57@gmail.com>
2024-01-23 00:25:40 +0600

    update purchase items

---
+++ backend/Chabagan.Fisheries/Chabagan.Fisheries.Data/Repositories/Stock/PurchaseItemRepo.cs
@@ -0,0 +1,126 @@
+﻿using AutoMapper;
+using Chabagan.Chabagan.Fisheries.DB;
+using Chabagan.Fisheries.Common.Constants;
+using Chabagan.Fisheries.Data.Repositories.Stock.Interfaces;
+using Chabagan.Fisheries.Entities.Mapping.Stock;
+using Chabagan.Fisheries.Entities.Models.Stock;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chabagan.Fisheries.Data.Repositories.Stock
+{
+    public class PurchaseItemRepo : IPurchaseItemRepo
+    {
+        #region Properties and Variables
+        /// <summary>
+        /// Database instance in this project
+        /// </summary>
+        private readonly FisheriesDbContext _dbContext;
+        /// <summary>
+        /// Intilize mapping profile
+        /// </summary>
+        private readonly IMapper _mapper;
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor that run the first time when calling this class
+        /// </summary>
+        /// <param name="dbContext"></param>
+        /// <param name="mapper"></param>
+        public PurchaseItemRepo(FisheriesDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+
+        #region Public Methods
+        /// <summary>
+        /// Get all purchase items data from database
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<VwPurchaseItem>> GetAllPurchaseItemsByPurchaseIdAsync(long purchaseId)
+        {
+            return _mapper.Map<IEnumerable<VwPurchaseItem>>(await _dbContext.PurchaseItems.Where(x => x.PurchaseId == purchaseId).AsNoTracking().ToListAsync());
+        }
+
+        /// <summary>
+        /// Get a single purchase item data from database by item id
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public async Task<VwPurchaseItem> GetPurchaseItemByItemIdAsync(long itemId)
+        {
+            return _mapper.Map<VwPurchaseItem>(await _dbContext.PurchaseItems.Where(x => x.Id == itemId)
+                .AsNoTracking()
+                .SingleOrDefaultAsync());
+        }
+
+        /// <summary>
+        /// Save Purchase item information in database
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<VwPurchaseItem> SavePurchaseItemAsync(VwPurchaseItem model)
+        {
+            if (model is null)
+                throw new ArgumentNullException(nameof(model));
+
+            DbPurchaseItem dbPurchaseItem = _mapper.Map<DbPurchaseItem>(model);
+            await _dbContext.PurchaseItems.AddAsync(dbPurchaseItem);
+            await _dbContext.SaveChangesAsync();
+            return await GetPurchaseItemByItemIdAsync(dbPurchaseItem.Id);
+        }
+
+
+        /// <summary>
+        /// Update Purchase Item information in database
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<VwPurchaseItem> UpdatePurchaseItemAsync(VwPurchaseItem model)
+        {
+            if (model is null)
+                throw new ArgumentNullException(nameof(model));
+
+            DbPurchaseItem? dbPurchaseItem = await _dbContext.PurchaseItems.Where(x => x.Id == model.Id).AsNoTracking().SingleOrDefaultAsync();
+
+            if (dbPurchaseItem is null)
+                throw new ArgumentNullException(ResponseMessage.FailRetrieve);
+
+            dbPurchaseItem = _mapper.Map<DbPurchaseItem>(model);
+            _dbContext.PurchaseItems.Update(dbPurchaseItem);
+            await _dbContext.SaveChangesAsync();
+            return await GetPurchaseItemByItemIdAsync(dbPurchaseItem.Id);
+        }
+
+
+        /// <summary>
+        /// Delete a Purchase Item data by item id
+        /// </summary>
+        /// <param name="purchaseId"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<VwPurchase> DeletePurchaseItemByItemIdAsync(long purchaseId)
+        {
+            if (purchaseId == 0)
+                throw new ArgumentNullException(ResponseMessage.BadRequest);
+
+            DbPurchaseItem? dbPurchaseItem = await _dbContext.PurchaseItems.Where(x => x.Id == purchaseId).AsNoTracking().SingleOrDefaultAsync();
+
+            if (dbPurchaseItem is null)
+                throw new ArgumentNullException(ResponseMessage.FailRetrieve);
+
+            _dbContext.PurchaseItems.Remove(dbPurchaseItem);
+            await _dbContext.SaveChangesAsync();
+            return new VwPurchase();
+        }
+
+        #endregion
+    }
+}
---
+++ backend/Chabagan.Fisheries/Chabagan.Fisheries.Entities/Mapping/Stock/VwPurchase.cs
@@ -0,0 +1,15 @@
+﻿namespace Chabagan.Fisheries.Entities.Mapping.Stock
+{
+    public class VwPurchase : VwBaseInfo
+    {
+        public DateTime RegDate { get; set; }
+        public long SupplierId { get; set; }
+        public string? Location { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal GrandTotal { get; set; }
+        public decimal PaidAmount { get; set; }
+        public decimal Dues { get; set; }
+        public bool ApprovalStatus { get; set; }
+    }
+}
